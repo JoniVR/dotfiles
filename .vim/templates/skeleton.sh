54fdef4731ae34b1c3ef9531eda5d60ebfa6c521
Joni Van Roost <joni.vanroost@student.kdg.be>
2019-04-17 20:37:10 +0200

    Update skeleton.sh
    - swap tabs with spaces for consistency
    - remove inconsistent spacing after ":"

--- .vim/templates/skeleton.sh
+++ .vim/templates/skeleton.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #============================================================
-#					FILE:  
-#	 DESCRIPTION: 
-#			 OPTIONS: 
-#	REQUIREMENTS: 
-#				AUTHOR: Joni Van Roost (joni.vr@hotmail.com)
-#			 VERSION: 1.0
-#			 CREATED:  
+#         FILE: 
+#  DESCRIPTION: 
+#      OPTIONS: 
+# REQUIREMENTS: 
+#       AUTHOR: Joni Van Roost (joni.vr@hotmail.com)
+#      VERSION: 1.0
+#      CREATED: 
 #============================================================
+
